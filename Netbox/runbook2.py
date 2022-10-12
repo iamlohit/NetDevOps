a423bdd2e18059121ac27f8a95b4b6f2dee5b512
iamlohit <jethwanilohit@gmail.com>
2022-10-12 08:59:33 +0530

    Using Nextbox as Network inventory to Automate Network Management

---
+++ Netbox/runbook2.py
@@ -0,0 +1,16 @@
+from nornir import InitNornir
+from nornir_scrapli.tasks import send_command
+from nornir_utils.plugins.functions import print_result
+import os
+from nornir.core import task
+
+nr = InitNornir(config_file="config.yaml")
+
+nr.inventory.defaults.username = os.environ["USERNAME"]
+nr.inventory.defaults.password = os.environ["PASSWORD"]
+
+def test_show_command(task):
+    task.run(task=send_command, command="show ip interface brief")
+
+results = nr.run(task=test_show_command)
+print_result(results)
